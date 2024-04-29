d2ff62e42d239ad9d18ef53c5b7a6e8dc580a8b1
Mariya Sapukhina <m.sapukhina@innopolis.university>
2024-04-29 23:02:43 +0300

    Fix backend tests.

--- backend/tests/test_backend.py
+++ backend/tests/test_backend.py
@@ -37,14 +37,15 @@ def test_search_books():
                           "&author=author1"
                           "&tags=tag1,tag2"
                           "&page=1"
-                          "&size=1")
+                          "&size=1",
+                          headers=test_auth_header)
     assert response.status_code == 200
     assert len(response.json()) > 0
 
 
 # Test if the endpoint to choose favorite books works as expected
 def test_choose_favorites():
-    response = client.post(f"/user/{test_username}/books",
+    response = client.post(f"/user/books",
                            json=[test_book_id],
                            headers=test_auth_header)
     assert response.status_code == 200
@@ -53,13 +54,15 @@ def test_choose_favorites():
 
 # Test if the endpoint to delete favorite books works as expected
 def test_delete_favorites():
-    response = client.delete(f"/user/{test_username}/books/{test_book_id}")
+    response = client.delete(f"/user/books/{test_book_id}",
+                             headers=test_auth_header)
     assert response.status_code == 200
     assert response.json() == {"message": "Favorite book deleted successfully"}
 
 
 # Test if the endpoint to get favorite books works as expected
 def test_get_favorites():
-    response = client.get(f"/user/{test_username}/books?brief=True")
+    response = client.get(f"/user/books?brief=True",
+                          headers=test_auth_header)
     assert response.status_code == 200
     assert len(response.json()) == 0
