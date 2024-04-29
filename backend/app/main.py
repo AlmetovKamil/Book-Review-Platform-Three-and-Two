2b8e2890c7110640ac1cddd0093e09e59046fae4
Aisinus <xxxAuronblade277xxx@gmail.com>
2024-04-30 00:12:10 +0500

    bandit start back skip

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,7 +18,9 @@ app = FastAPI()
 
 def start():
     print("Starting service...")
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+
+    uvicorn.run("app.main:app",
+                host="0.0.0.0", port=8000, reload=True)  # nosec
 
 
 if __name__ == "__main__":
