86821a740490fb640a20738c68f9068ddd194755
Mariya Sapukhina <m.sapukhina@innopolis.university>
2024-04-29 23:16:52 +0300

    Add tokens to frontend tests.

--- frontend/tests/test_frontend.py
+++ frontend/tests/test_frontend.py
@@ -1,13 +1,27 @@
 import datetime
 
 import pytest
+import jwt
 from streamlit.testing.v1 import AppTest
 
 from app.models.user import User
 
-test_user = User(username="test_user", created_at=datetime.datetime.today())
+
+def create_access_token(payload):
+    secret_key = 'test_secret_key'
+    algorithm = 'HS256'
+
+    access_token = jwt.encode(payload, secret_key, algorithm=algorithm)
+    return access_token
+
+
+test_username = "test_user@gmail.com"
+test_token = create_access_token({"username": test_username})
+
+test_user = User(username=test_username, created_at=datetime.datetime.today())
 at = AppTest.from_file("../app/main.py")
 at.session_state["user"] = test_user
+at.session_state["token"] = {"id_token": test_token}
 at.run()
 
 
