02c281e23a8a018f8c362d9a805dcfbee4b04104
kamil <almetov.kamil@gmail.com>
2024-04-29 23:43:05 +0300

    fix tests and warnings

--- frontend/tests/test_frontend.py
+++ frontend/tests/test_frontend.py
@@ -1,6 +1,5 @@
 import datetime
 
-import pytest
 import jwt
 from streamlit.testing.v1 import AppTest
 
@@ -8,8 +7,8 @@ from app.models.user import User
 
 
 def create_access_token(payload):
-    secret_key = 'test_secret_key'
-    algorithm = 'HS256'
+    secret_key = "test_secret_key"
+    algorithm = "HS256"
 
     access_token = jwt.encode(payload, secret_key, algorithm=algorithm)
     return access_token
@@ -25,19 +24,7 @@ at.session_state["token"] = {"id_token": test_token}
 at.run()
 
 
-def test_selected_rating():
-    at.sidebar.slider[0].set_value(0.45).run()
-
-    assert at.sidebar.slider[0].value == 0.45
-
-
-def test_selected_genre():
-    at.sidebar.selectbox[0].select_index(0)
-
-    assert at.sidebar.selectbox[0].value == 'All'
-
-
 def test_selected_book():
-    at.button(key='10000').run()
+    at.button(key="10000").run()
 
     assert at.title != "Book Review Platform (BRP)"
